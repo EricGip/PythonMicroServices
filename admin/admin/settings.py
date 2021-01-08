08b67f8a0fdb033cde6265a8f7dd1efb9ad33312
eric <ericgip97@gmail.com>
2021-01-07 17:30:04 -0800

    removing keys

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'products'
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'admin',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306',
+    },
 }
 
 
